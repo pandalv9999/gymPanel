c48ec3bb9a9f6593d22424c4c45416fd2bcbe8e0
Rujun Yao <pandalv9999@gmail.com>
2020-10-15 16:49:46 -0700

    add some criteia to make sure the update/delete operation are save.

--- controller/courseController.js
+++ controller/courseController.js
@@ -15,13 +15,19 @@ module.exports = (app) => {
         void client.connect((err, db) => {
             if (err) throw err;
             void client.db(process.env.database).collection("courses").updateOne(
-                {id: req.body.courseId},
+                {id: req.body.courseId, $expr: {$lt: [{$size: "$enrolledMember"}, "$capacity"]}},
                 {$push: {enrolledMember: req.body.username}}, (err, result) => {
-                    if (err) res.sendStatus(409);
-                    console.log(req.body.username + "successfully registered course " + req.body.courseId);
-                    res.sendStatus(200)
-                });
+                    if (err) throw err;
+                    if (result.modifiedCount === 0) {
+                        res.sendStatus(409);
+                        console.log(req.body.username + " fail registered course " + req.body.courseId);
+                    } else {
+                        console.log(req.body.username + " successfully registered course " + req.body.courseId);
+                        res.sendStatus(200)
+                    }
+                }); // using $expr to prevent when two user register simultaneously, all of then will registered.
             // todo: if user's profile is set up, need to add the course from user's courseTaken field
+            // todo: if user has appointment with trainer, the register step will fail.
         });
     });
 
@@ -31,11 +37,16 @@ module.exports = (app) => {
         void client.connect((err, db) => {
             if (err) throw err;
             void client.db(process.env.database).collection("courses").updateOne(
-                {id: req.body.courseId},
+                {id: req.body.courseId, enrolledMember: req.body.username},
                 {$pull: {enrolledMember: req.body.username}}, (err, result) => {
-                    if (err) res.sendStatus(409);
-                    console.log(req.body.username + "successfully unregistered course " + req.body.courseId);
-                    res.sendStatus(200)
+                    if (err) throw err;
+                    if (result.modifiedCount === 0) {
+                        res.sendStatus(409);
+                        console.log(req.body.username + " fail unregistered course " + req.body.courseId);
+                    } else {
+                        console.log(req.body.username + " successfully unregistered course " + req.body.courseId);
+                        res.sendStatus(200)
+                    }
                 });
             // todo: if user's profile is set up, need to delete the course from user's courseTaken field
         });
