7fc37787fb21f8f29b0bedf119123ee757e04418
Rujun Yao <pandalv9999@gmail.com>
2020-10-18 20:34:53 -0700

    add serveral arrays that required for course registeration in the register process

--- controller/mainController.js
+++ controller/mainController.js
@@ -9,12 +9,12 @@ const url = 'mongodb+srv://' +
     process.env.password + '@testdb.qzr4t.mongodb.net/' +
     process.env.database + '?retryWrites=true&w=majority';
 // The second argument in MongoClient is an object for not getting the deprecation warnings from MongoDB
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
 
 module.exports = (app) => {
 
     // This router returns the main page for the application
-    app.get('/index',  (req, res) => {
+    app.get('/index', (req, res) => {
         console.log("receiving get request for the home page");
         res.writeHead("200", {'content-Type': 'text/HTML'});
         fs.createReadStream(__dirname + "/../public/index.html").pipe(res)
@@ -28,56 +28,61 @@ module.exports = (app) => {
 
     // This router lets user send their register info in the body of the request
     app.post('/create-data', (req, res) => {
-      // Sending request to create a data
-      void client.connect((err, db) => {
-          if (err) throw err;
+        // Sending request to create a data
+        void client.connect((err, db) => {
+            if (err) throw err;
 
-          // check if the username or email has been used
-          client.db(process.env.database).collection("users").findOne({
-            $or: [{
-                email: req.body.email
-            }, {
-                username: req.body.username
-            }]
-        }).then(user => {
-            if (user) {
-                let errors = {};
-                if (user.username === req.body.username) {
-                    errors.username = "Username already exists, please go back to choose another username or directly login";
+            // check if the username or email has been used
+            client.db(process.env.database).collection("users").findOne({
+                $or: [{
+                    email: req.body.email
+                }, {
+                    username: req.body.username
+                }]
+            }).then(user => {
+                if (user) {
+                    let errors = {};
+                    if (user.username === req.body.username) {
+                        errors.username = "Username already exists, please go back to choose another username or directly login";
+                    } else {
+                        errors.email = "Email already exists, please go back to choose another email or directly login";
+                    }
+                    res.status(400).json(errors);
                 } else {
-                    errors.email = "Email already exists, please go back to choose another email or directly login";
+                    // if the username or email has not been used, then create a new user
+                    return client.db(process.env.database).collection("users").insertOne(
+                        {
+                            username: req.body.username,
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName,
+                            email: req.body.email,
+                            phone: req.body.phone,
+                            DOB: req.body.username,
+                            gender: req.body.gender,
+                            weight: req.body.weight,
+                            height: req.body.height,
+                            registeredCourses: [],
+                            scheduledAppointments: [],
+                            scheduledTime: [[], [], [], [], [], [], []]
+
+
+                        }, function (err, info) {
+                            if (err) throw err;
+                            console.log("Successfully create an account!");
+                            //res.send("Successfully create an account!");
+                            //res.json(info.ops)
+                            res.redirect('/');
+                        });
                 }
-                res.status(400).json(errors);
-            } else {
-                // if the username or email has not been used, then create a new user
-                return client.db(process.env.database).collection("users").insertOne (
-                    {
-                    username: req.body.username, 
-                    firstName: req.body.firstName,
-                    lastName: req.body.lastName,
-                    email: req.body.email,
-                    phone: req.body.phone,
-                    DOB: req.body.username,
-                    gender: req.body.gender,
-                    weight: req.body.weight,
-                    height: req.body.height 
-                }, function (err, info) {
-                    if (err) throw err;
-                    console.log("Successfully create an account!");
-                    //res.send("Successfully create an account!");
-                    //res.json(info.ops)
-                    res.redirect('/');
+            })
+                .catch(err => {
+                    return res.status(500).json({
+                        error: err
+                    });
                 });
-            }
-           })
-           .catch(err => {
-            return res.status(500).json({
-                error: err
-            });
-          });
         });
     });
-    
+
 
     // This router handle the get request from the front-end to handle the log-in features
     // Note that I use get request for simplicity. Post request will be more appropriate in the current situation.
@@ -90,16 +95,15 @@ module.exports = (app) => {
             // find whether current user is existed in the database.
             void client.db(process.env.database).collection("users").findOne({"username": username},
                 (err, result) => {
-                if (err) throw err;
-                if (!result) {
-                    console.log("User " + req.params.username + " login failed! No user matches.");
-                    res.render('error', {errorCode: 403, errorMessage: "The user " + username + " does not exist"});
-                }
-                else {
-                    console.log("User " + req.params.username + " login success.");
-                    res.render('dashboard', {user: result})
-                }
-            });
+                    if (err) throw err;
+                    if (!result) {
+                        console.log("User " + req.params.username + " login failed! No user matches.");
+                        res.render('error', {errorCode: 403, errorMessage: "The user " + username + " does not exist"});
+                    } else {
+                        console.log("User " + req.params.username + " login success.");
+                        res.render('dashboard', {user: result})
+                    }
+                });
         });
     });
 };
