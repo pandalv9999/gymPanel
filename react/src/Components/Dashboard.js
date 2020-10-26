711bb849986c6c4d3b946f4425593cf3f94538e5
rujunyao <pandalv9999@gmail.com>
2020-10-25 19:59:08 -0700

    basic styling for the dashboard

--- react/src/Components/Dashboard.js
+++ react/src/Components/Dashboard.js
@@ -7,27 +7,37 @@ import './style/Dashboard.css';
 
 const Dashboard = ({user, userSetter}) => {
 
-    const [currentModule, setCurrentModule] = useState("");
+    const [currentModule, setCurrentModule] = useState(<Schedule user={user}/>);
+    const [currentState, setCurrentState] = useState([true, false, false, false]);
     const currentPage = "#";
+
+    const setCurrentPage = (index, module) => {
+      setCurrentModule(module);
+      const states = [false, false, false, false];
+      states[index] = true;
+      setCurrentState(states);
+    };
 
     return (
         <div className={"main-container"}>
             <aside className={"side-nav"}>
                 <h2>Welcome<br/> {user.username}</h2>
                 <ul className="list">
-                    <li key="schedule" style={{listStyleType : "None"}}>
+                    <li key="schedule" style={{listStyleType : "None"}} className={currentState[0] && "active"}>
                         <a href={currentPage} className="App-link"
-                           onClick={() => setCurrentModule(<Schedule user={user}/>)}>Schedule</a>
+                           onClick={() => setCurrentPage(0, <Schedule user={user}/>)}>Schedule</a>
                     </li>
-                    <li key="profile" style={{listStyleType : "None"}}>
+                    <li key="profile" style={{listStyleType : "None"}} className={currentState[1] && "active"}>
                         <a href={currentPage} className="App-link"
-                           onClick={() => setCurrentModule(<Profile user={user}/>)}>Profile</a>
+                           onClick={() => setCurrentPage(1, <Profile user={user}/>)}>Profile</a>
                     </li>
-                    <li key="courses" style={{listStyleType : "None"}}>
-                        <a href={currentPage} className="App-link" onClick={() => setCurrentModule(<Courses user={user}/>)}>Courses</a>
+                    <li key="courses" style={{listStyleType : "None"}} className={currentState[2] && "active"}>
+                        <a href={currentPage} className="App-link"
+                           onClick={() => setCurrentPage(2, <Courses user={user}/>)}>Courses</a>
                     </li>
-                    <li key="trainers" style={{listStyleType : "None"}}>
-                        <a href={currentPage} className="App-link" onClick={() => setCurrentModule(<Trainers user={user}/>)}>Trainers</a>
+                    <li key="trainers" style={{listStyleType : "None"}} className={currentState[3] && "active"}>
+                        <a href={currentPage} className="App-link"
+                           onClick={() => setCurrentPage(3, <Trainers user={user}/>)}>Trainers</a>
                     </li>
                     <li key="log-out" style={{listStyleType : "None"}}>
                         <a href={currentPage} className="App-link" onClick={() => {
