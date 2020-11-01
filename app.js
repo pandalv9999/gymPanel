236ab6f47569640bb9e334616fc5a33a2db15580
rujunyao <pandalv9999@gmail.com>
2020-11-01 13:30:56 -0800

    fix

--- app.js
+++ app.js
@@ -22,6 +22,12 @@ dashboardController(app);
 courseController(app, utils);
 trainerController(app, utils);
 
+// create a way for the express to serve react home page
+app.get('*', (req, res) => {
+  console.log("sending react homepage");
+  res.sendFile(__dirname+'/react/build/index.html');
+});
+
 //listen to a specific port.
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Start listening for the port ${process.env.PORT}` );
