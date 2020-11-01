aa40ed350f402a414cf83cb410d14880901c75ea
yiman <yimanliu0722@gmail.com>
2020-11-01 04:09:57 -0500

    change eslint config and format code with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,26 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
+    node: true,
   },
-  extends: "eslint:recommended",
+  extends: [
+    "eslint:recommended",
+    "prettier",
+    // "plugin:react/recommended"
+  ],
   parserOptions: {
-    ecmaVersion: 12,
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   rules: {
-    indent: ["error", 4],
+    indent: ["warn", 2, { SwitchCase: 1 }],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
+    "no-console": 0,
   },
 };
